faedbbda4e2b170e0d75d8b04a11da51f3ff884b
Matisse A <matisss.alll@proton.me>
2026-09-09 22:30:07 +0200

    debugged functions (ZigZag w/ Zig Zag Matrix Diagonal Traversal online & CDT) + new prints

--- CDTMatriceV1.py
+++ CDTMatriceV1.py
@@ -5,9 +5,8 @@ Matisse A.
 v1
 """
 
-import math as ma
+from math import cos, pi, sqrt
 
-#Matrice Base (donnée dans le TD)
 Mbase = [[70, 70, 100, 70, 87, 87, 150, 187],
          [85, 100, 96, 79, 87, 154, 87, 113],
          [100, 85, 116, 79, 70, 87, 86, 196],
@@ -56,18 +55,19 @@ def f(u,v, base):
 
 def c(k):
     if k == 0 :
-        return 1/ma.sqrt(2) 
+        return 1/sqrt(2) 
     else:
         return 1
 
 def CDT(u,v, matrice):
     fcos = 0
-    x, y, N = c(u), c(v), len(matrice)
+    N = len(matrice)
     for n in range(N):
         for m in range(N):
-            fcos += f(n,m, matrice)*ma.cos(((2*n+1)*u*ma.pi)/(2*N))*ma.cos(((2*m+1)*v*ma.pi)/(2*N))
+            #f(n,m, matrice) to (f(n,m, matrice) - 128) because the range of a pixel in JPEG is from [-128,127] instead of [0,255]
+            fcos += ((f(n,m, matrice) - 128)*cos(((2*n+1)*u*pi)/(2*N))*cos(((2*m+1)*v*pi)/(2*N)))   
 
-    return round((2/N)*x*y*fcos)
+    return round((2/N)*c(u)*c(v)*fcos)
 
 
 
@@ -100,56 +100,29 @@ def Cq(matrice):
 def ZigZag(matrice):
     """
     Le principe est de lire en zig zag à partir des coordonées (ici sous forme de positions dans la liste de liste)
-
-    Voilà à quoi ressemble la lecture en zig zag acutelle (1ere partie) :
-
-    (0,0) 
-    (1,0) (0,1) => Tuple pair
-    (0,2) (1,1) (2,0) => Tuple impair
-    (3,0) (2,1) (1,2) (0,3) => Tuple pair
-    (0,4) (1,3) (2,2) (3,1) (4,0) => ...
-    (5,0) (4,1) (3,2) (2,3) (1,4) (0,5) 
-    (0,6) (1,5) (2,4) (3,3) (4,2) (5,1) (6,0)
-    (7,0) (6,1) (5,2) (4,3) (3,4) (2,5) (1,6) (0,7)
-
-    """
-    coord = (0,0)
-    liste = []
-    max = 0
-    for i in range(len(matrice)):
-        max +=1
-        for j in range(max):
-            #Pour les "Tuple pair"
-            if max%2 == 0 or max == 1:
-                coord = (j,max-j-1)
-            #Pour les "Tuple impair"
-            else:
-                coord = (i-j,j)
-            liste.append(cq[coord[0]][coord[1]])
     
+    Idea took from Zig Zag Matrix Diagonal Traversal online
     """
-    2eme boucle, pour l'autre moitié du carré :
-
-    (1,7) (2,6) (3,5) (4,4) (5,3) (6,2) (7,1)
-    (7,2) (6,3) (5,4) (4,5) (3,6) (2,7)
-    (3,7) (4,6) (5,5) (6,4) (7,3)
-    (7,4) (6,5) (5,6) (4,7)
-    (5,7) (6,6) (7,5)
-    (7,6) (6,7)
-    (7,7)
-
-    """    
-    max = len(matrice)
-    for i in range(len(matrice)):
-        max -=1
-        for j in range(max,0,-1):
-            if max%2 == 0 or max == 1:
-                coord = (j+i,len(matrice)-j)
-            else:
-                coord = (len(matrice)-j,j+i)
-            liste.append(cq[coord[0]][coord[1]])
-
-    return liste
+    N = len(matrice)
+    resultat = []
+    for s in range(2 * N - 1):
+        if s % 2 == 0:
+            # Remonter en diagonale
+            r = min(s, N - 1)
+            c = s - r
+            while r >= 0 and c < N:
+                resultat.append(matrice[r][c])
+                r -= 1
+                c += 1
+        else:
+            # Descendre en diagonale
+            c = min(s, N - 1)
+            r = s - c
+            while c >= 0 and r < N:
+                resultat.append(matrice[r][c])
+                r += 1
+                c -= 1
+    return resultat
             
             
 
@@ -180,7 +153,6 @@ def RLC(liste):
         while j != len(liste) and liste[j] == char:
             nb+=1
             j+=1
-        print(nb, end=", ")
         retliste.append([nb,char])
     
     return retliste
@@ -207,38 +179,27 @@ def huffmann():
 
 
 
+
+
+def print_matrix(title, matrice):
+    print(f"\n=== {title} ===")
+    for line in matrice:
+        print(" ".join(f"{val:4d}" for val in line))
+           
 cq = Cq(Mbase)
+cdt_obtenue = CDTmatrice(Mbase)
+zigzag_cq = ZigZag(cq)
+sans_zigzag_cq = SansZigZag(cq)
 
+print_matrix("CDT OBTENUE", cdt_obtenue)
+print_matrix("RÉSULTAT VOULU (TD)", ResVoulu)
+print_matrix("MATRICE QUANTIFIÉE Cq", cq)
 
+print("\n=== COMPARISON RLC (ZIGZAG vs SANS ZIGZAG) ===")
+print(f"Nombre de paires RLC AVEC ZigZag : {len(RLC(zigzag_cq))}")
+print(f"Nombre de paires RLC SANS ZigZag : {len(RLC(sans_zigzag_cq))}")
 
-
-
-go = True       
-if go:            
-    print("\n-------CDT DE LA MATRICE-------")
-    print("Matrice obtenue : ")
-    print(CDTmatrice(Mbase),"\n")
-    print("Matrice voulu : ")
-    print(ResVoulu,"\n")
-    print("\n")
-
-
-    print("-------CQ(u,v)-------")
-    print(Cq(Mbase),"\n")
-    print("\n")
-
-
-    print("-------LECTURE ZIG ZAG-------")
-    print(ZigZag(cq),"\n")
-    print("Lecture sans Zig Zag")
-    print(SansZigZag(cq))
-    print("\n")
-
-
-    print("-------RLC ET PROBA DU ZIG ZAG-------")
-    print(proba(RLC(ZigZag(Mbase))),"\n")
-    print("\n")
-
-
-    print("-------RLC ET PROBA SANS ZIG ZAG-------") #Car j'ai l'impression de me tromper quelque part : Sans le ZigZag, le RLC à l'air plus efficace.
-    print(proba(RLC(SansZigZag(cq))),"\n")
+print("\n=== RLC + PROBABILITÉS (ZIGZAG) ===")
+print("Format: [Occurrences, Valeur, Probabilité]")
+for item in proba(RLC(zigzag_cq)):
+    print(item)
